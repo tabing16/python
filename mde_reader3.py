7118235473c84ada3982f8b20eb5973ab4e83b32
tabing16 <tabing16@users.noreply.github.com>
2019-06-25 16:49:21 +0800

    Add files via upload

--- mde_reader3.py
+++ mde_reader3.py
@@ -1,7 +1,9 @@
 import struct
 import math
+import os
 from datetime import datetime
 from datetime import timedelta
+import re
 
 
 class AutoVivication(dict):
@@ -17,13 +19,19 @@ def decode_chars(chars):
     return chars.decode('utf-8')
 
 
-with open("MV90_MDEF_LT_20190325124256_3820.MDE", "rb") as f:
+def convert_dt(chars):
+    dt = re.findall(r'^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})$',chars)
+    return dt[0][2]+"-"+dt[0][1]+"-"+dt[0][0]+" "+dt[0][3]+":"+dt[0][4]
+
+mv90filename = "MV90_MDEF_LT_20190325124256_3820.MDE"
+
+with open(os.path.join("mde", mv90filename), "rb") as f:
     a = f.read()
 
 i = 1
 recs = []
-print(len(a))
-
+#print(len(a))
+arr = []
 while i <= len(a) / 216:
     j = 216 * i
     if j <= 216:
@@ -34,35 +42,38 @@ while i <= len(a) / 216:
     recs.append(arr)
     arr = []
 
-print(recs)
+#print(recs)
 av = AutoVivication()
 
 for rec in recs:
     mychn = {}
     myrdg = {}
     z = 0
-    meter = None
-    datestart = None
-    dateend = None
-    chnstat = None
-    intstat = None
+   
+   
     for comp in rec:
         n = 24
         if len(comp[0:2]) == 2:
             a = struct.unpack('<H', comp[2:4])[0]
             if a == 1:
-                meter = decode_chars(comp[4:24])
+                meter = decode_chars(comp[4:24])[0:10]
                 datestart = decode_chars(comp[119:131])
-                #print(datestart)
                 datestop = decode_chars(comp[131:143])
-                #print(datestop)
             elif a == 10:
-                chnstat = decode_chars(comp[99:100])
-                #print(chnstat)
-                intstat = decode_chars(comp[100:101])
-                #print(intstat)
-                # if chnstat == 'Y' and intstat == 'Y':
-                #     n = 48
+                # chnstat = decode_chars(comp[99:100])
+                # intstat = decode_chars(comp[100:101])
+                int_per_hour = decode_chars(comp[177:179])
+                chan_number = decode_chars(comp[93:95])
+                datetime_strp = "%d-%m-%Y %H:%M"
+                datestart_strp = datetime.strptime(convert_dt(datestart), datetime_strp)
+                if int_per_hour == '02':
+                    datestart_strp = datestart_strp + timedelta(minutes=29)
+                elif int_per_hour == '04':
+                    datestart_strp = datestart_strp + timedelta(minutes=14)
+
+                datestart_strf = datetime.strftime(datestart_strp,datetime_strp)
+
+                print(f"Meter : {meter}, Channel: {chan_number}, DateStart: {datestart_strf}, DateStop: {convert_dt(datestop)}, Interval Per Hour: {int_per_hour}")             
             elif 1001 <= a <= 9998:
                 z = z - 1
                 b = 0
@@ -73,43 +84,6 @@ for rec in recs:
                         av[z][b] = round(struct.unpack('<f', comp[n:n+4])[0],3)
                     n += 8
                     b += 1
+            
         z += 1
-
-# av = AutoVivication()
-# mychn = {}
-# myrdg = {}
-# z = 0
-# datestart = None
-# dateend = None
-# chnstat = None
-# intstat = None
-# for comp in arr:
-#     n = 24
-#     if len(comp[0:2]) == 2:
-#         a = struct.unpack('<H', comp[2:4])[0]
-#         if a == 1:
-#             datestart = decode_chars(comp[119:131])
-#             print(datestart)
-#             datestop = decode_chars(comp[131:143])
-#             print(datestop)
-#         elif a == 10:
-#             chnstat = decode_chars(comp[99:100])
-#             print(chnstat)
-#             intstat = decode_chars(comp[100:101])
-#             print(intstat)
-#             # if chnstat == 'Y' and intstat == 'Y':
-#             #     n = 48
-#         elif 1001 <= a <= 9998:
-#             z = z - 1
-#             b = 0
-#             while n <= 192:
-#                 if(math.isnan(struct.unpack('<f', comp[n:n+4])[0])):
-#                     pass
-#                 else:
-#                     av[z][b]= round(struct.unpack('<f', comp[n:n+4])[0],3)
-#                 n += 8
-#                 b += 1
-#     z += 1
-
-# print(av)
-# print(n)
+        
